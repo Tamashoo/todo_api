558fab72221c33ae407593f55b88634739457c76
Tamashoo <tamasho-tomoya-ch@ynu.jp>
2023-08-05 20:12:33 +0900

    add task

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -42,13 +42,13 @@ func (uc *userController) Login(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, err.Error())
 	}
 	//tokenString, err := uc.uu.Login(user)
-	_, err := uc.uu.Login(user)
+	tokenString, err := uc.uu.Login(user)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, err.Error())
 	}
 	cookie := new(http.Cookie)
 	cookie.Name = "token"
-	cookie.Value = ""
+	cookie.Value = tokenString
 	cookie.Expires = time.Now().Add(24 * time.Hour)
 	cookie.Path = "/"
 	cookie.Domain = os.Getenv("API_DOMAIN")
